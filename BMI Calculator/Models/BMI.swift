f3cb51e176d5b20ef4d8771022055fac79ca5530
NachoLJ <nacholopjim@gmail.com>
2021-03-17 13:14:10 +0100

    first commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Ignacio Lopez Jimenez on 15/11/2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+//Hay que cambiar el import a UIKit para tener acceso a UIColor
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
